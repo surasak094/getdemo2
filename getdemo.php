dd14b20bba3be04388a4e392b17aab3ba4b0d007
surasak094 <87976146+surasak094@users.noreply.github.com>
2021-10-02 00:41:22 +0700

    Add files via upload

--- getdemo.php
+++ getdemo.php
@@ -1,21 +1,20 @@
 <?php
 //Creates new record as per request
     //Connect to database
-    $servername = "mysql-52539-0.cloudclusters.net";
-    $username = "admin";
-    $password = "Gg7Ok1pX";
-    $dbname   = "SurasakDB";
-    $dbServerPort = "18850";
-    
+    $servername = "us-cdbr-east-04.cleardb.com";
+    $username = "bc06134d8f3e1f";
+    $password = "550425d9";
+    $dbname = "heroku_78638205065f537";
+
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname, $dbServerPort,);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
         die("Database Connection failed: " . $conn->connect_error);
     }
 
     //Get current date and time
-    date_default_timezone_set('Asia/Bangkok');
+    date_default_timezone_set('Asia/Bangkok'');
     $d = date("Y-m-d");
     //echo " Date:".$d."<BR>";
     $t = date("H:i:s");
